fd460913d032fafc4897ec5a41f671de18e7dc3c
Carlos Romero <romero.ca20042028@gmail.com>
2026-05-03 13:07:07 -0500

    add footer

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,9 +4,10 @@ import { Hero } from '../../components/hero/hero';
 import { WhatsappButton } from '../../components/whatsapp-button/whatsapp-button';
 import { About } from '../../components/about-us/about-us';
 import { Product } from '../../components/product/product';
+import { Footer } from '../../components/footer/footer';
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero, WhatsappButton, About, Product],
+  imports: [Navbar, Hero, WhatsappButton, About, Product, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
