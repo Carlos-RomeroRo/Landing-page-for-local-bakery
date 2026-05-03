96f741fca28712e13c0a4caac0ca9d9622959c95
Carlos Romero <romero.ca20042028@gmail.com>
2026-05-03 12:27:14 -0500

    add new section (product)

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,9 +3,10 @@ import { Navbar } from '../../components/navbar/navbar';
 import { Hero } from '../../components/hero/hero';
 import { WhatsappButton } from '../../components/whatsapp-button/whatsapp-button';
 import { About } from '../../components/about-us/about-us';
+import { Product } from '../../components/product/product';
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero, WhatsappButton, About],
+  imports: [Navbar, Hero, WhatsappButton, About, Product],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
